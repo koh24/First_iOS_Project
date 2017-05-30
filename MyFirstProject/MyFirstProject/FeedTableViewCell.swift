39115cce1df12b89ae0d1fddf44d9938c6d30141
koh24 <Rattapong.ma@gmail.com>
2017-05-29 19:46:40 -0700

    Create Project
    -Added Icon
    -Added Project File

---
+++ MyFirstProject/MyFirstProject/FeedTableViewCell.swift
@@ -0,0 +1,39 @@
+//
+//  FeedTableViewCell.swift
+//  MyFirstProject
+//
+//  Created by IndyZa on 5/29/2560 BE.
+//  Copyright © 2560 IndyZaLab. All rights reserved.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var iconImgView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var accessoryLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func setupUI(news:News){
+        self.iconImgView.image = news.iconImage
+        self.titleLabel.text = news.title
+        self.descriptionLabel.text = news.description
+        self.accessoryLabel.text = "\(news.author) • \(news.view) views • \(news.createDate)"
+        self.typeLabel.text = news.type
+    }
+    
+}
